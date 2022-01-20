94f5bed32d0ec295489bd9e19bd5e401159b7d2c
Iryna Shandar <iryna.shandar@gmail.com>
2022-01-21 00:51:53 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-// App.js
 import './App.css';
+import React, { Component } from 'react';
+import Searchbar from './Components/Searchbar';
+import ImageGallery from './Components/ImageGallery';
 
-function App() {
-  return <div className="App">TEST</div>;
+class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  submitForm = form => {
+    this.setState({ imageName: form });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.submitForm} />
+        <ImageGallery imageName={this.state.imageName} />
+      </div>
+    );
+  }
 }
 
 export default App;
